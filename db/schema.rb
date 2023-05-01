95e149d71441746764dbfbfe74fbe7ee9eaae618
hisaju <hisaju@gmail.com>
2023-05-01 22:22:50 +0900

    イベント作成画面

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_01_114656) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_01_125129) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "title"
+    t.string "image"
+    t.string "place"
+    t.datetime "start_at"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_events_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
@@ -24,4 +36,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_01_114656) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "events", "users"
 end
