4dfb2c9fdd68d84ffd6bdddd93ed16f32b8c3515
hisaju <hisaju@gmail.com>
2023-05-01 21:16:35 +0900

    会員登録

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+
+  # User registration
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    # If user is saved successfully than show flash message
+    if @user.save
+      flash[:notice] = "User created successfully"
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
